281f683ee0472c13f21516355316925cc1966f95
uenoryo <uenoryo@uma-b.com>
2019-11-10 21:41:30 +0900

    yml parseまで.

---
+++ src/Deserializer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Umab\Swagger;
+
+use SplFileInfo;
+use DomainException;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Deserializer はswaggerの定義ファイルをロードし、Swaggerオブジェクトに変換して返す
+ */
+class Deserializer implements DeserializerInterface
+{
+    /**
+     * swaggerの定義ファイルをロードし、Swaggerオブジェクトに変換して返す
+     *
+     * @param string $filepath 定義ファイル
+     * @todo .yml しか対応していないので .json にも対応させる
+     */
+    public function deserialize(string $filepath)
+    {
+        $info = new SplFileInfo($filepath);
+        $ext = $info->getExtension();
+
+        switch ($ext) {
+            case "yml":
+            case "yaml":
+                return $this->deserializeYml($filepath);
+            default:
+                throw new DomainException('unsupported file type: '.$ext);
+        }
+    }
+
+    protected function deserializeYml(string $filepath)
+    {
+        $yml = Yaml::parse(file_get_contents($filepath));
+        print_r($yml);
+    }
+}
