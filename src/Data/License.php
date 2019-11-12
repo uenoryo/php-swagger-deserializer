78b534bd14ecd4ba2d8f8611b6f2509c12837bf7
uenoryo <uenoryo@uma-b.com>
2019-11-12 21:43:12 +0900

    add License.

---
+++ src/Data/License.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Umab\Swagger\Data;
+
+use Umab\Swagger\Util\Readable;
+
+/**
+ * License ...
+ */
+class License
+{
+    use Readable;
+
+    /** @var data */
+    protected $data;
+
+    /** @var name */
+    protected $name;
+
+    /** @var url */
+    protected $url;
+
+    public function __construct(?array $data = [])
+    {
+        $this->data = $data;
+
+        $this->name = $data['name'] ?? "";
+
+        $this->url = $data['url'] ?? "";
+    }
+}
