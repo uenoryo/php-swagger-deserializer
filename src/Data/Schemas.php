12c323746e453e32673b6ef7b895e684b7e2b81a
uenoryo <uenoryo@uma-b.com>
2019-11-12 23:56:30 +0900

    array accessを継承する.

--- src/Data/Schemas.php
+++ src/Data/Schemas.php
@@ -2,13 +2,14 @@
 
 namespace Umab\Swagger\Data;
 
+use ArrayObject;
 use IteratorAggregate;
 use Umab\Swagger\Util\Readable;
 
 /**
  * Schemas ...
  */
-class Schemas implements IteratorAggregate
+class Schemas extends ArrayObject implements IteratorAggregate
 {
     use Readable;
 
@@ -45,6 +46,11 @@ class Schemas implements IteratorAggregate
                     $this->schemasByName[$name] = new Schema($schema);
             }
         }
+    }
+
+    public function offsetGet($name)
+    {
+        return $this->schemasByName[$name] ?? null;
     }
 
     function getIterator()
